3770b2ee6abc0fd23d44f33b806b64df8156ec68
Ashley Chontos <ashleychontos@gmail.com>
2021-08-15 12:43:16 -1000

    Minor version update to use as tag for release

--- sortasurvey/__init__.py
+++ sortasurvey/__init__.py
@@ -8,7 +8,7 @@ from .utils import *
 
 __all__ = ['cli', 'observing', 'pipeline', 'sample', 'survey', 'utils']
 
-__version__ = '1.0.2'
+__version__ = '1.1.0'
 
 _ROOT = os.path.abspath(os.getcwd())
 INPDIR = os.path.join(_ROOT, 'info')
